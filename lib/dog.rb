2262160e7d0196110b445c73760293692ab21e38
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-06 05:53:21 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @@all << name
+
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.all
+    @@all.each{|dog| puts dog}
+  end
+end
